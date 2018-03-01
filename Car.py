7d73332f615f1bcfd8e5bc0a247e8e724cabf09c
Sean Lewis <sean.alan.lewis@gmail.com>
2018-03-01 20:29:38 +0000

    Added some new functionality

--- Car.py
+++ Car.py
@@ -2,57 +2,83 @@ class Car:
     # Models a car's state
     curPos = {'x': None, 'y': None}
     desPos = {'x': None, 'y': None}
+    currentRide = None
     hasRide = False
+    pickingUp = False
 
     def __init__(self, name):
-      self.name = name
+        self.name = name
 
     # String output of Car for debug
     def __str__(self):
-      toOutput = "\n" + self.name + " is at ("+self.x()+","+self.y()+ ") and "
-      if(self.hasRide): toOutput += " has a ride."
-      else: toOutput += " doesn't have a ride."
-      if(self.desPos["x"] != None):
-        toOutput += "\nIts headed towards ("+self.destX()+","+self.destY()+")."
-      return toOutput
+        toOutput = "\n" + self.name + \
+            " is at ("+self.x()+","+self.y() + ") and "
+        if(self.hasRide):
+            toOutput += " has a ride."
+        else:
+            toOutput += " doesn't have a ride."
+        if(self.desPos["x"] != None):
+            toOutput += "\nIts headed towards (" + \
+                self.destX()+","+self.destY()+")."
+        return toOutput
+
+    # Assigns stuff
+    def assign(self, ride):
+        self.currentRide = ride
+        self.pickingUp = True
+        self.hasRide = False
+        self.setDest(ride.startX(), ride.startY())
 
     # Returns the number of steps it will take to get to some location
     def timeTo(self, x, y):
-      return abs(self.x - x) + abs(self.y - y)
+        return abs(self.x() - x) + abs(self.y() - y)
 
     # Returns true if the destination matches the position
     def isAtDestination(self):
-      return (self.x == self.destX) and (self.y == self.destY)
-      
+        return (self.x() == self.destX()) and (self.y() == self.destY())
+
     # Once called, moves the Car one step towards its destination
     def updatePos(self):
-      if(self.x != self.destX): 
-        self.curPos["x"] = self.x+1 if (self.x < self.destX) else self.x-1
-      else:
-        self.curPos["y"] = self.y+1 if (self.y < self.destY) else self.y-1
+        if(self.x != self.destX):
+            self.curPos["x"] = self.x()+1 if (self.x() <
+                                              self.destX()) else self.x-1
+        else:
+            self.curPos["y"] = self.y()+1 if (self.y() <
+                                              self.destY()) else self.y-1
+
+        # Do some checks
+        if(self.pickingUp and self.isAtDestination()):
+            self.pickingUp = False
+            self.hasRide = True
+            self.setDest(self.currentRide.destX(), self.currentRide.destY())
+
+        if(self.hasRide and self.isAtDestination()):
+            self.pickingUp = False
+            self.hasRide = False
+            self.currentRide = None
 
     # Get x position
     def x(self):
-      return self.curPos["x"]
+        return self.curPos["x"]
 
     # Get y position
     def y(self):
-      return self.curPos["y"]
+        return self.curPos["y"]
 
     # Get destination x position
     def destX(self):
-      return self.desPos["x"]
+        return self.desPos["x"]
 
     # Get destination y position
     def destY(self):
-      return self.desPos["y"]      
+        return self.desPos["y"]
 
     # Given an x and y value, set x and y
     def setPos(self, x, y):
-      self.curPos["x"] = x
-      self.curPos["y"] = y
+        self.curPos["x"] = x
+        self.curPos["y"] = y
 
     # Given an x and y value, set x and y destination positions
     def setDest(self, x, y):
-      self.desPos["x"] = x
-      self.desPos["y"] = y
+        self.desPos["x"] = x
+        self.desPos["y"] = y
--- Ride.py
+++ Ride.py
@@ -1,39 +1,40 @@
 class Ride:
-	start = {'x': None, 'y': None}
-	end = {'x': None, 'y': None}
-	earliestStart = None
-	latestFinish = None
+    start = {'x': None, 'y': None}
+    end = {'x': None, 'y': None}
+    earliestStart = None
+    latestFinish = None
 
-	def __init__(self, name, startX, startY, endX, endY, earliest, finish):
-		self.name = name
-		self.setStart(startX, startY)
-		self.setEnd(endX, endY)
-		self.earliestStart = earliest
-		self.latestFinish = finish
+    def __init__(self, name, startX, startY, endX, endY, earliest, finish):
+        self.name = name
+        self.setStart(startX, startY)
+        self.setEnd(endX, endY)
+        self.earliestStart = earliest
+        self.latestFinish = finish
 
-	def __str__(self):
-		toOutput = "\n"+self.name
-		toOutput += " starts @ ("+self.startX()+","+self.startY() + "), "
-		toOutput += "\nends @ ("+self.endX()+","+self.endY() + ") and "
-		toOutput += " must finish before " + self.latestFinish + "."
-		return toOutput
+    def __str__(self):
+        return ("\n"+self.name + " starts @ ("+self.startX() + ","+self.startY()
+                + "), " + "\nends @ ("+self.endX() + ","+self.endY() + ") and "
+                        + " must finish before " + self.latestFinish + ".")
 
-	def startX(self):
-		return self.start["x"]
+    def distance(self):
+        return abs(self.startX()-self.endX()) + abs(self.startY()-self.endY())
 
-	def startY(self):
-		return self.start["y"]
+    def startX(self):
+        return self.start["x"]
 
-	def endX(self):
-		return self.end["x"]
+    def startY(self):
+        return self.start["y"]
 
-	def endY(self):
-		return self.end["y"]
+    def endX(self):
+        return self.end["x"]
 
-	def setStart(self, x, y):
-		self.start["x"] = x
-		self.start["y"] = y
+    def endY(self):
+        return self.end["y"]
 
-	def setEnd(self, x, y):
-		self.end["x"] = x
-		self.end["y"] = y
+    def setStart(self, x, y):
+        self.start["x"] = x
+        self.start["y"] = y
+
+    def setEnd(self, x, y):
+        self.end["x"] = x
+        self.end["y"] = y
