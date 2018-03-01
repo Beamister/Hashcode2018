b972fc3c13d1808fac9b8a55c3a6f4409386b2d6
Sean Lewis <sean.alan.lewis@gmail.com>
2018-03-01 19:35:24 +0000

    Added additional functionality and comments

--- Car.py
+++ Car.py
@@ -16,25 +16,43 @@ class Car:
         toOutput += "\nIts headed towards ("+self.destX()+","+self.destY()+")."
       return toOutput
 
+    # Returns the number of steps it will take to get to some location
     def timeTo(self, x, y):
       return abs(self.x - x) + abs(self.y - y)
 
+    # Returns true if the destination matches the position
+    def isAtDestination(self):
+      return (self.x == self.destX) and (self.y == self.destY)
+      
+    # Once called, moves the Car one step towards its destination
+    def updatePos(self):
+      if(self.x != self.destX): 
+        self.curPos["x"] = self.x+1 if (self.x < self.destX) else self.x-1
+      else:
+        self.curPos["y"] = self.y+1 if (self.y < self.destY) else self.y-1
+
+    # Get x position
     def x(self):
       return self.curPos["x"]
 
+    # Get y position
     def y(self):
       return self.curPos["y"]
 
+    # Get destination x position
     def destX(self):
       return self.desPos["x"]
 
+    # Get destination y position
     def destY(self):
       return self.desPos["y"]      
 
+    # Given an x and y value, set x and y
     def setPos(self, x, y):
       self.curPos["x"] = x
       self.curPos["y"] = y
 
+    # Given an x and y value, set x and y destination positions
     def setDest(self, x, y):
       self.desPos["x"] = x
       self.desPos["y"] = y
